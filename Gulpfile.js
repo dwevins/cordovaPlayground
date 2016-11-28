539cd71265fe7b1bb6b57734414975ed2514f09e
Danny Evins <woody.evins@gmail.com>
2016-11-28 12:09:30 -0600

    working on css minification

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
+
 
 gulp.task('styles', function() {
     gulp.src('www/sass/index.scss')
@@ -7,7 +9,14 @@ gulp.task('styles', function() {
         .pipe(gulp.dest('www/css/'));
 });
 
+gulp.task('minify-css', function() {
+  return gulp.src('www/css/index.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('www/css/min.css'));
+});
+
 //Watch task
 gulp.task('default',function() {
     gulp.watch('www/sass/*.scss',['styles']);
+    gulp.watch('www/css/index.css', ['minify-css']);
 });
